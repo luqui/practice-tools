2b8a574c24d288ed15d5dfc5a37f956415438fec
Luke Palmer <lrpalmer@gmail.com>
2019-06-04 18:00:40 -0400

    Gutted web/, preparing for ghcjs version

---
+++ JSTools.js
@@ -0,0 +1,66 @@
+PracticeTools = function($) {
+
+var $$ = {};
+
+$$.Event = function() {
+  var listeners = [];
+  return {
+    listen: cb => listeners.push(cb),
+    fire: x => {
+      listeners.map(cb => cb(x));
+    }
+  };
+};
+
+$$.MIDIInterface = function() {
+  var indevice = null;
+  var outdevice = null;
+
+  var setDeviceList = function(sel, devs, onchange) {
+    sel.empty();
+    sel.append($('<option>').attr('value', null).text("None"));
+    for (var i = 0; i < devs.length; i++) {
+      sel.append(
+        $('<option>').attr('value', i).text(devs[i].name));
+    }
+    sel.change(onchange);
+  };
+
+  var indevs = $('<select>');
+  var outdevs = $('<select>');
+  var widget = $('<div>').append(
+    $('<span>').text('In:'), indevs,
+    $('<span>').text('Out:'), outdevs);
+
+  var inevent = $$.Event();
+
+  navigator.requestMIDIAccess().then(access => {
+    var inputs = Array.from(access.inputs.values());
+    var outputs = Array.from(access.outputs.values());
+    setDeviceList(indevs, inputs, () => {
+      indevice = inputs[indevs.val()];
+      if (indevice) {
+        indevice.onmidimessage = msg => {
+          inevent.fire(msg);
+        };
+      }
+    });
+    setDeviceList(outdevs, outputs, () => {
+      outdevice = outputs[outdevs.val()];
+    });
+
+    // Debug TODO remember
+    indevs.val(1).change();
+    outdevs.val(1).change();
+  });
+
+  return {
+    widget: widget,
+    sendMIDI: data => { 
+      if (outdevice) { outdevice.send(data) } 
+    },
+    event: inevent
+  };
+};
+
+};
