79c0ab7c624d270bcf930c877f0d203a7e36b07e
Luke Palmer <lrpalmer@gmail.com>
2019-06-04 21:24:36 -0400

    Migrated listen.hs over to JS! (it's broken)

--- EarTraining.hs
+++ EarTraining.hs
@@ -1,56 +1,243 @@
 {-# OPTIONS_GHC -Wall #-}
-{-# LANGUAGE BlockArguments #-}
+{-# LANGUAGE MultiWayIf, ScopedTypeVariables #-}
 
-import Prelude hiding ((!!))
-import Control.Arrow (first)
-import Control.Concurrent (forkIO)
-import Control.Monad (forM_, forever, void)
-import Control.Monad.IO.Class (liftIO)
-import Data.Bits ((.&.))
-import Data.IORef
+import Prelude hiding (seq)
+import qualified JSMIDI as MIDI
+import Data.List (isPrefixOf, tails, delete, nub, sortBy, permutations)
 import Data.Tuple (swap)
-import qualified Deque.Strict as Deque
-import Language.Javascript.JSaddle
+import qualified Data.Set as Set
+import qualified Data.Map as Map
+import Control.Concurrent (threadDelay)
+import Control.Monad (filterM, forever, replicateM, join, when, forM_)
+import Control.Monad.IO.Class (liftIO)
+import qualified Control.Monad.Trans.State as State
+import qualified Control.Monad.Random as Rand
+import Data.Ord (comparing)
+import System.IO (hFlush, stdout)
+import qualified Language.Javascript.JSaddle as JS
 
-main :: JSM ()
+type Cloud = Rand.Rand Rand.StdGen
+type Connections = (MIDI.Input, MIDI.Output)
+
+
+getConn :: JS.JSM Connections
+getConn = MIDI.makeInterface
+
+combinations :: Int -> [a] -> [[a]]
+combinations 0 _ = [[]]
+combinations n [] = []
+combinations n (x:xs) = map (x:) (combinations (n-1) xs) ++ combinations n xs
+
+shuffle :: [a] -> Cloud [a]
+shuffle [] = pure []
+shuffle xs = do
+    n <- Rand.uniform (zipWith const [0..] xs)
+    ((xs !! n) :) <$> shuffle (take n xs ++ drop (n+1) xs)
+
+choose :: Int -> [a] -> Cloud [a]
+choose n xs = take n <$> shuffle xs
+
+increasingExercises :: Cloud [[[Int]]]
+increasingExercises = Rand.uniform range >>= \(n0 :: Int) -> go [n0]
+    where
+    go :: [Int] -> Cloud [[[Int]]]
+    go notes = concat <$> sequenceA [ pure [[[head notes]]], goLine notes, goChord notes, (go . (: notes) =<< Rand.uniform (foldr delete range notes)) ]
+
+    goLine :: [Int] -> Cloud [[[Int]]]
+    goLine notes = nub . sortBy (comparing length) . deconstruct 3 . map (:[]) <$> shuffle notes
+
+    goChord :: [Int] -> Cloud [[[Int]]]
+    goChord notes = map (:[]) <$> choose 4 (combinations 3 notes)
+            
+
+    range = [55..67]
+
+    deconstruct :: Int -> [a] -> [[a]]
+    deconstruct lmin xs
+        | length xs <= lmin = [xs]
+        | otherwise = deconstruct lmin pre ++ deconstruct lmin post ++ [xs]
+        where
+        (pre,post) = (take (length xs `div` 2) xs, drop (length xs `div` 2) xs)
+
+
+
+
+noteSequence :: Int -> Cloud Int -> Cloud Int -> Cloud [[Int]]
+noteSequence size baseNote intervals = map (:[]) <$> byIntervals size baseNote intervals
+
+fanCloud :: Cloud a -> Cloud a
+fanCloud m = Rand.evalRand m <$> Rand.getSplit
+
+evalRandJS :: Cloud a -> JS.JSM a
+evalRandJS = liftIO . Rand.evalRandIO
+
+main :: JS.JSM ()
 main = do
-    tools <- jsg1 "JSTools" =<< jsg "jQuery"
-    midi <- tools # "MIDIInterface" $ ()
-    void $ (jsg1 "jQuery" =<< (jsg "document" ! "body")) # "append" $ (midi ! "widget")
+    conns <- getConn
+    scoredGame conns =<< evalRandJS (chunkerleave 10 <$> sequenceA (map fanCloud [rowGame, chordGame, intervalGame]))
+
+interleave :: [[a]] -> Cloud [a]
+interleave [] = pure []
+interleave xss = do
+    n <- Rand.uniform $ zipWith const [0..] xss
+    case xss !! n of
+        [] -> interleave (take n xss ++ drop (n+1) xss)
+        (x:xs) -> (x:) <$> interleave (take n xss ++ [xs] ++ drop (n+1) xss)
+
+chunkerleave :: Int -> [[a]] -> [a]
+chunkerleave _ [] = []
+chunkerleave n ([]:xss) = chunkerleave n xss
+chunkerleave n (xs:xss) = take n xs ++ chunkerleave n (xss ++ [drop n xs])
+
+filterCloud :: (a -> Bool) -> Cloud a -> Cloud a
+filterCloud p c = do
+    x <- c
+    if p x
+        then pure x
+        else filterCloud p c
+
+scale :: Int -> Cloud [Int]
+scale baseNote = Rand.uniform . concatMap (\s -> [s, map (subtract 12) (reverse s)]) . map (scanl (+) baseNote) . concat $ 
+                    [ take 7 . map (take 7) . tails . cycle $ major, take 7 . map (take 7) . tails . cycle $ melodic ]
+    where
+    major = [2,2,1,2,2,2,1]
+    melodic = [2,1,2,2,2,2,1]
+
+chord :: (Eq a) => Int -> Cloud a -> Cloud [a]
+chord 0 _ = pure []
+chord n notecloud = do
+    note <- notecloud
+    (note :) <$> chord (n-1) (filterCloud (/= note) notecloud)
+
+
+byIntervals :: Int -> Cloud Int -> Cloud Int -> Cloud [Int]
+byIntervals notes baseNote intervals = scanl (+) <$> baseNote <*> replicateM (notes-1) intervals
+        
+
+sequenceRound :: Connections -> [[Int]] -> JS.JSM Bool
+sequenceRound (src,dest) chords = do
+    let playNotes =
+            forM_ chords $ \notes -> do
+                forM_ notes $ \note -> MIDI.sendEvent dest $ MIDI.NoteOn 0 note 64
+                threadDelay 250000
+                forM_ notes $ \note -> MIDI.sendEvent dest $ MIDI.NoteOn 0 note 0
+
+    let expected = reverse $ map Set.fromList chords
+
+    let listenSuccess history win = do
+            ch <- listenChord (src,dest)
+            if | ch == Set.singleton 108 -> playNotes >> listenSuccess [] (null history)  -- hear again, valid unless you have already started the answer
+               | expected `isPrefixOf` (ch:history) -> pure win
+               | length (ch:history) < length expected -> listenSuccess (ch:history) win
+               | otherwise -> listenSuccess (ch:history) False
+
+    playNotes
+    listenSuccess [] True
+
+data ScoreStats = ScoreStats
+    { ssScore :: Int
+    , ssRunLength :: Int
+    , ssDebt :: Map.Map [[Int]] Int
+    , ssLives :: Int
+    }
+
+scoredGame :: Connections -> [[[Int]]] -> JS.JSM ()
+scoredGame conns = go (ScoreStats 0 0 Map.empty 4)
+    where
+    go score exes 
+      | ssLives score == 0 = putStrLn "Game Over"
+      | otherwise = do
+        putStrLn $ "Level: " ++ show (ssScore score) 
+              ++ " | Debt: " ++ show (sum (ssDebt score))
+              ++ " | Run:  " ++ show (ssRunLength score)
+              ++ " | Lives: " ++ show (ssLives score)
+        threadDelay 500000
+
+        (round,adv) <- evalRandJS . Rand.weighted $ ((exes !! ssScore score, True), 5) : [ ((ex, False), fromIntegral w) | (ex,w) <- Map.assocs (ssDebt score), w > 0 ]
+        winround <- sequenceRound conns round
+        let score' = ScoreStats
+                       { ssScore = if winround && adv then ssScore score + 1 else ssScore score
+                       , ssRunLength = if winround then ssRunLength score + 1 else 0
+                       , ssDebt = if winround then Map.alter (addDebt (-1)) round (ssDebt score)
+                                              else Map.alter (addDebt 2) round (ssDebt score)
+                       , ssLives = if | winround && (ssRunLength score `mod` 10) == 9 -> ssLives score + 1
+                                      | winround -> ssLives score
+                                      | otherwise -> ssLives score - 1
+                        }
+        go score' exes
     
-    poller <- makeMIDIPoller midi
+    addDebt x Nothing | x > 0 = Just x
+    addDebt x (Just y) | x + y > 0 = Just (x+y)
+    addDebt _ _ = Nothing
 
-    void . liftIO . forkIO . forever $ do
-        void waitForAnimationFrame
-        maybee <- poller 
-        forM_ maybee $ \e -> do
-            doc <- jsg "document"
-            _ <- doc # "write" $ toJSVal (toJSString (show e))
-            pure ()
+rowGame :: Cloud [[[Int]]]
+rowGame = increasingExercises
 
-    pure ()
+scaleGame :: Cloud [[[Int]]]
+scaleGame = mapM pickScale [60..]
+    where
+    pickScale baseNote = do 
+        s <- scale baseNote
+        s' <- scale (last s)
+        pure $ map (:[]) (s <> tail s') 
 
-data MIDIEvent = NoteOn Int Int Int  -- channel, key, vel normalized with NoteOff = NoteOn 0
-    deriving (Show)
+intervalGame :: Cloud [[[Int]]]
+intervalGame = mapM pickPair (concatMap (replicate 3) [50..])
+    where
+    pickPair range0 = do
+        bass <- Rand.uniform [36..48]
+        topnote <- Rand.uniform [range0..range0+12]
+        pure [[bass, topnote]]
 
-convertEvent :: JSVal -> JSM (Maybe MIDIEvent)
-convertEvent v = do
-    dat <- v ! "data"
-    d0 <- fromJSValUnchecked =<< dat !! 0
-    d1 <- fromJSValUnchecked =<< dat !! 1
-    d2 <- fromJSValUnchecked =<< dat !! 2
-    let ch = d0 .&. 0x0f
-    pure $ case d0 .&. 0xf0 of
-        0x80 -> Just $ NoteOn ch d1 0
-        0x90 -> Just $ NoteOn ch d1 d2
-        _ -> Nothing
-    
+chordGame :: Cloud [[[Int]]]
+chordGame = mapM pickChord (concatMap (replicate 3) [50..])
+    where
+    pickChord range0 = do
+        basenote <- Rand.uniform [0..11]
+        quality <- Rand.uniform
+            [ [ 0, 4, 7 ]  -- maj
+            , [ 0, 3, 7 ]  -- min
+            , [ 0, 3, 6 ]  -- dim
+            , [ 0, 4, 8 ]  -- aug
+            , [ 0, 4, 7, 11 ]  -- maj7
+            , [ 0, 4, 7, 10 ]  -- 7
+            --, [ 0, 4, 6, 11 ]  -- maj7 b5
+            --, [ 0, 4, 6, 10 ]  -- 7 b5
+            , [ 0, 3, 7, 11 ]  -- maj min
+            , [ 0, 3, 7, 11 ]  -- maj min b5
+            , [ 0, 3, 7, 10 ]  -- min 7
+            --, [ 0, 3, 6, 10 ]  -- min 7 b5
+            , [ 0, 3, 6, 9  ]  -- dim7
+            --, [ 0, 4, 8, 11 ]  -- maj7 #5
+            --, [ 0, 4, 8, 10 ]  -- +7
+            ]
+        let normalize n = (n `mod` 12) + range0
+        pure [map (normalize . (+ basenote)) quality]
 
-makeMIDIPoller :: JSVal -> JSM (JSM (Maybe MIDIEvent))
-makeMIDIPoller interface = do
-    queue <- liftIO $ newIORef mempty
-    void $ (interface ! "event") # "listen" $ fun \_ _ [e] -> do
-        maybee' <- convertEvent e
-        forM_ maybee' $ liftIO . modifyIORef queue . Deque.snoc
-    pure . liftIO $ atomicModifyIORef queue (\q -> swap (maybe (Nothing, q) (first Just) (Deque.uncons q)))
 
+listenChord :: Connections -> JS.JSM (Set.Set Int)
+listenChord (src, dest) = listenOn Set.empty
+    where
+    listenOn :: Set.Set Int -> JS.JSM (Set.Set Int)
+    listenOn notes = do
+        (note, vel) <- waitNote
+        if | vel == 0 -> listenOff notes (Set.delete note notes)
+           | otherwise -> listenOn (Set.insert note notes)
+
+    listenOff :: Set.Set Int -> Set.Set Int -> JS.JSM (Set.Set Int)
+    listenOff notes remnotes
+        | Set.null remnotes = pure notes
+        | otherwise = do
+            (note, vel) <- waitNote
+            if | vel == 0 -> listenOff notes (Set.delete note remnotes)
+               | otherwise -> listenOff notes remnotes -- ignore note presses after first release (?)
+
+    waitNote :: JS.JSM (Int, Int)
+    waitNote = do
+        e <- getNextEvent (src,dest)
+        case e of
+            Just (MIDI.NoteOn _ note vel) -> pure (note,vel)
+            Nothing -> threadDelay 10000 >> waitNote
+
+getNextEvent :: Connections -> JS.JSM (Maybe (MIDI.Event))
+getNextEvent (src, dest) = MIDI.pollEvent src
---
+++ JSMIDI.hs
@@ -0,0 +1,66 @@
+{-# OPTIONS_GHC -Wall #-}
+{-# LANGUAGE BlockArguments #-}
+module JSMIDI where
+
+import Prelude hiding ((!!))
+import Control.Arrow (first)
+import Control.Concurrent (forkIO)
+import Control.Monad (forM_, forever, void)
+import Control.Monad.IO.Class (liftIO)
+import Data.Bits ((.&.), (.|.))
+import Data.IORef
+import Data.Tuple (swap)
+import qualified Deque.Strict as Deque
+import Language.Javascript.JSaddle
+
+newtype Input = Input { pollEvent :: JSM (Maybe Event) }
+
+newtype Output = Output { sendEvent :: Event -> JSM () }
+
+makeInterface :: JSM (Input, Output)
+makeInterface = do
+    tools <- jsg1 "JSTools" =<< jsg "jQuery"
+    midi <- tools # "MIDIInterface" $ ()
+    void $ (jsg1 "jQuery" =<< (jsg "document" ! "body")) # "append" $ (midi ! "widget")
+    
+    poller <- makeMIDIPoller midi
+
+    void . liftIO . forkIO . forever $ do
+        void waitForAnimationFrame
+        maybee <- poller 
+        forM_ maybee $ \e -> do
+            doc <- jsg "document"
+            void $ doc # "write" $ toJSVal (toJSString (show e))
+
+    let send e = do
+            dat <- toJSData e
+            void $ midi # "send" $ dat
+
+    pure (Input poller, Output send)
+
+data Event = NoteOn Int Int Int  -- channel (0 based), key, vel normalized with NoteOff = NoteOn 0
+    deriving (Show)
+
+fromJSData :: JSVal -> JSM (Maybe Event)
+fromJSData v = do
+    dat <- v ! "data"
+    d0 <- fromJSValUnchecked =<< dat !! 0
+    d1 <- fromJSValUnchecked =<< dat !! 1
+    d2 <- fromJSValUnchecked =<< dat !! 2
+    let ch = d0 .&. 0x0f
+    pure $ case d0 .&. 0xf0 of
+        0x80 -> Just $ NoteOn ch d1 0
+        0x90 -> Just $ NoteOn ch d1 d2
+        _ -> Nothing
+    
+toJSData :: Event -> JSM JSVal
+toJSData (NoteOn ch k v) = toJSVal [0x90 .|. ch, k, v]
+
+makeMIDIPoller :: JSVal -> JSM (JSM (Maybe Event))
+makeMIDIPoller interface = do
+    queue <- liftIO $ newIORef mempty
+    void $ (interface ! "event") # "listen" $ fun \_ _ [e] -> do
+        maybee' <- fromJSData e
+        forM_ maybee' $ liftIO . modifyIORef queue . Deque.snoc
+    pure . liftIO $ atomicModifyIORef queue (\q -> swap (maybe (Nothing, q) (first Just) (Deque.uncons q)))
+
