4a10dc1a3c1e62fa283c1633bbf1b6b35060045d
Luke Palmer <lrpalmer@gmail.com>
2019-07-04 16:01:15 -0500

    Removed ear-training app.  I think I'm just going to use a webview.

--- ear-training/assets/split-mp3.py
+++
@@ -1,7 +0,0 @@
-import os
-
-for note in range(49):
-    t0 = note * 2
-    cmd = 'ffmpeg -i piano-c2-c6-2sec.mp3 -ss {} -t 2 piano-note{}-2sec.mp3'.format(t0, note+36)
-    print cmd
-    os.system(cmd)
